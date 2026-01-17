d75a2a3f27bada64e5ff0fd66deffaddf6559760
link-short <dora.family228@gmail.com>
2026-01-17 13:26:53 +0500

    Create index.js

---
+++ OAuth/index.js
@@ -0,0 +1,31 @@
+// Импортируем нужные функции из SDK 2026 года
+  import { initializeApp } from "https://www.gstatic.com";
+  import { getDatabase, ref, set, push } from "https://www.gstatic.com";
+
+  // Ваши данные из консоли Firebase (Settings -> General)
+  const firebaseConfig = {
+    apiKey: "AIzaSyBFKk4qECXkvtezmrIuCMx23QryP8nyQ48",
+    authDomain: "winnigames2024app.firebaseapp.com",
+    databaseURL: "https://winnigames2024app-default-rtdb.firebaseio.com",
+    projectId: "winnigames2024app",
+    storageBucket: "winnigames2024app.firebasestorage.app",
+    messagingSenderId: "ID",
+    appId: "1:496367700072:android:d69c5ae5c3b007e32613dd"
+  };
+
+  // Инициализация
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+
+  // Функция для сохранения текста
+  window.saveText = function(textValue) {
+    const messagesRef = ref(db, 'messages'); // Путь в базе данных
+    const newMessageRef = push(messagesRef); // Создаем уникальный ID для записи
+    
+    set(newMessageRef, {
+      text: textValue,
+      timestamp: Date.now()
+    })
+    .then(() => alert("You're logined!"))
+    .catch((error) => console.error("Ошибка:", error));
+  }
