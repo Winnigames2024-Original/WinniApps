32d57d107306c5e21d2514abe7d30b1d2a2736b6
link-short <dora.family228@gmail.com>
2026-01-18 17:31:06 +0500

    Update index.js

--- download-appCaptcha/index.js
+++ download-appCaptcha/index.js
@@ -1,15 +1,9 @@
 const urlParamsllJhfds = new URLSearchParams(window.location.search);
 const codDeE = urlParamsllJhfds.get('code');
-const siteKey = urlParamsllJhfds.get('site-key');
 
 if (codDeE) {
   if (codDeE == "captcha-verified") {
     const redirectUrl = urlParamsllJhfds.get('redirect_url');
     window.location.href = redirectUrl;
   }
-}
-
-if (siteKey) {
-  const reCaptchaButtonDiv = document.getElementById('reCaptchaButton');
-  reCaptchaButtonDiv.insertAdjacentHTML('beforeend', '<div class="g-recaptcha" data-sitekey="' + siteKey + '" data-callback="done"></div>');
 }
